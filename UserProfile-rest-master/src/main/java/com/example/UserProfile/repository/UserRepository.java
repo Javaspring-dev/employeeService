173953a0849033c8cc44b6b0f93c48f121e37e5b
Javaspring-dev <jtraining32@gmail.com>
2024-11-27 23:08:56 +0530

    Delete UserProfile-rest-master directory

--- UserProfile-rest-master/src/main/java/com/example/UserProfile/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.UserProfile.repository;
-
-import com.example.UserProfile.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
